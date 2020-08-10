f01c8d46640d5f1fa2f3d46afeeaf2ddc29b5629
omegga <whymagnet@gmail.com>
2020-08-10 23:10:50 +0200

    retrieve cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    args.next(); // ignore 1st arg
+    if let Some(command) = args.next() {
+        run_command(command);
+    } else {
+        println!("no argument provided");
+    }
+}
+
+fn run_command(command: String) {
+    if command == "-d" {
+        run_download_test();
+    } else if command == "-u" {
+        run_upload_test();
+    } else {
+        println!("invalid argument");
+    }
+}
+
+fn run_download_test() {
+    println!("run download test");
+}
+
+fn run_upload_test() {
+    println!("run upload test");
 }

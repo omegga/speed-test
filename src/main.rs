28b870aa414ca07c3109bb5eaacf49c9a367fb4a
omegga <whymagnet@gmail.com>
2020-08-11 23:03:06 +0200

    refacto speed test

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,37 @@
 use flate2::read::GzDecoder;
-use std::env;
+use serde_json::Value;
+use std::error::Error;
 use std::fs;
 use std::process::Command;
 use std::str;
 use tar::Archive;
 
 const SPEEDTEST_URL: &str = "https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-1.0.0-x86_64-linux.tgz";
+const TARBALL: &str = "speedtest.tgz";
+const SPEEDTEST_BIN: &str = "speedtest";
 
 fn main() {
-    let mut args = env::args();
-    args.next(); // ignore 1st arg
-    if let Some(command) = args.next() {
-        run_command(command);
-    } else {
-        println!("no argument provided");
-    }
+    test_speed();
 }
 
-fn run_command(command: String) {
-    if command == "-d" {
-        let dest = "speedtest.tgz";
-        download_speedtest(dest).expect("error downloading speedtest");
-        extract_speedtest(dest).expect("error extracting speedtest");
-        run_speedtest("./speedtest");
-        run_download_test();
-    } else if command == "-u" {
-        run_upload_test();
-    } else {
-        println!("invalid argument");
-    }
+fn test_speed() {
+    download_speedtest_cli(TARBALL).expect("error downloading speedtest");
+    extract_speedtest_cli(TARBALL).expect("error extracting speedtest");
+    let command = format!("./{} -f json", SPEEDTEST_BIN);
+    let json_result = run_speedtest_cli(&command).expect("error running speedtest");
+    println!("...done\n");
+    print_speedtest_results(&json_result).expect("error reading results");
 }
 
-fn run_download_test() {
-    println!("run download test");
-}
-
-fn run_upload_test() {
-    println!("run upload test");
-}
-
-fn download_speedtest(dest: &str) -> Result<(), Box<dyn std::error::Error>> {
+fn download_speedtest_cli(filename: &str) -> Result<(), Box<dyn Error>> {
+    println!("\ndownloading speedtest...");
     let speedtest = reqwest::blocking::get(SPEEDTEST_URL)?.bytes()?;
-    fs::write(dest, speedtest)?;
-    println!("Done");
+    fs::write(filename, speedtest)?;
     Ok(())
 }
 
-fn extract_speedtest(filename: &str) -> Result<(), std::io::Error> {
+fn extract_speedtest_cli(filename: &str) -> Result<(), std::io::Error> {
+    println!("extracting speedtest...");
     let tar_gz = fs::File::open(filename)?;
     let tar = GzDecoder::new(tar_gz);
     let mut archive = Archive::new(tar);
@@ -54,12 +39,37 @@ fn extract_speedtest(filename: &str) -> Result<(), std::io::Error> {
     Ok(())
 }
 
-fn run_speedtest(speedtest: &str) {
+fn run_speedtest_cli(command: &str) -> Result<String, Box<dyn Error>> {
+    println!("running speedtest...");
     let output = Command::new("sh")
         .arg("-c")
-        .arg(speedtest)
+        .arg(command)
         .output()
         .expect("failed to execute speedtest");
-    let text = str::from_utf8(&output.stdout).expect("failed to convert output");
-    println!("{}", text);
+    Ok(String::from_utf8(output.stdout).expect("failed to convert output"))
+}
+
+fn print_speedtest_results(json_results: &str) -> Result<(), Box<dyn Error>> {
+    let v: Value = serde_json::from_str(json_results)?;
+    let download = v["download"]["bandwidth"].as_f64().unwrap();
+    let upload = v["upload"]["bandwidth"].as_f64().unwrap();
+    let latency = v["ping"]["latency"].as_f64().unwrap();
+    let isp = v["isp"].as_str().unwrap();
+    let server_ip = v["server"]["ip"].as_str().unwrap();
+    let server_country = v["server"]["country"].as_str().unwrap();
+    let server_location = v["server"]["location"].as_str().unwrap();
+    println!("isp: {}", isp);
+    println!("download: {} Mbps", convert_bits_to_megabits(download));
+    println!("upload: {} Mbps", convert_bits_to_megabits(upload));
+    println!("latency: {} ms", format!("{:.2}", latency));
+    println!(
+        "server: {}",
+        format!("{}, {} {}", server_location, server_country, server_ip)
+    );
+    Ok(())
+}
+
+fn convert_bits_to_megabits(bits: f64) -> String {
+    let megabits = bits / (125000 as f64);
+    format!("{:.2}", megabits)
 }
